437b4a8d086c0cc7d36f30f36539897502570737
Loogeek <xiaolong3956@gmail.com>
2024-01-14 23:42:26 +0800

    init project & add button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    class="v-button"
+    :class="{
+      [`v-button--${type}`]: type,
+      [`v-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <slot />
+  </button>
+</template>
+<script setup lang="ts">
+import type {ButtonProps} from './types'
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+})
+
+// export default {
+  
+// }
+</script>
+<style lang="">
+  
+</style>
